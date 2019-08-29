4a0cc8f1efe89756159b17b3979e46bc18643c8e
Maxim Khaylenko <dev.mobile.fl@gmail.com>
2019-08-29 07:22:12 +0300

    minor improvements

--- app/src/main/java/com/vuz/task/domain/PasswordUtil.kt
+++ app/src/main/java/com/vuz/task/domain/PasswordUtil.kt
@@ -7,10 +7,10 @@ class PasswordUtil {
 
         //todo uncomment
         //at least 1 char lower case, 1 char upper case, 1 number, 1 special character
-        //val REGEX_PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$&+,:;=\\\\?@#|/'<>.^*()%!-]).+$"
+        val REGEX_PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$&+,:;=\\\\?@#|/'<>.^*()%!-]).+$"
 
         //at least 1 char lower case, 1 char upper case, 1 number
-        val REGEX_PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).+$"
+        //val REGEX_PASSWORD = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).+$"
 
         fun isPasswordMatches(login: String): Boolean {
             return Pattern.matches(REGEX_PASSWORD, login)
